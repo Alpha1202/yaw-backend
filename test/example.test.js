41c3269551835cea52e3378458ad162858cc801b
Nzubechukwu <nzubennamani@gmail.com>
2019-11-09 00:06:19 +0100

    fixed heroku deployment bug

--- test/example.test.js
+++ test/example.test.js
@@ -4,24 +4,21 @@ import chaiHttp from 'chai-http';
 
 const USER_API = '/api/v1/users';
 
-
+chai.should();
 chai.use(chaiHttp);
 const server = () => chai.request(app);
 
-// describe('Example Test', () => {
-//   it('should expect true === true', () => {
-//     expect('true').to.be.eql('true');
-//   });
-// });
 
-describe('App', () => {
-  it('Should start the server', (done) => {
-    server().get('/').end((error, res) => {
-    expect(res.status).equal(200);
-    expect(res.body).to.have.property('Welcome')
+describe('server', () => {
+  it('should start the server successfully', done => {
+    chai
+      .request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.status).equal(200);
+   expect(res.body).to.have.property('Welcome')
     expect(res.body.Welcome).equal('Welcome to YAW')
-    })
-    server().close(done)
-  })
-
-})
+        done();
+      });
+  });
+});
